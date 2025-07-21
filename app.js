bbd7760c00c21ba9c8b82aa5024744d3d797fe95
Dnyanu Sonawane <dnyasonawane5804@gmail.com>
2025-07-21 22:39:13 +0530

    google url for sign in

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:9000/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:9000/auth/google/callback"
 },
   async (accessToken, refreshToken, profile, done) => {
     try {
